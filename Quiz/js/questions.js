0e5c5edc1cb02df9d9b2d60266169bed48da06b1
Gautamsingh45 <gautamsingh893591@gmail.com>
2023-04-19 12:42:23 +0530

    change7

--- Quiz/js/questions.js
+++ Quiz/js/questions.js
@@ -58,15 +58,225 @@ let questions = [
   // you can uncomment the below codes and make duplicate as more as you want to add question
   // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
 
-  //   {
-  //   numb: 6,
-  //   question: "Your Question is Here",
-  //   answer: "Correct answer of the question is here",
-  //   options: [
-  //     "Option 1",
-  //     "option 2",
-  //     "option 3",
-  //     "option 4"
-  //   ]
-  // },
+  {
+    numb: 6,
+    question: "Which of the tag is used to attach css file in html document?",
+    answer: "link",
+    options: [
+      "link",
+      "css",
+      "script",
+      "html"
+    ]
+  },
+  {
+    numb: 7,
+    question: "Which tag is used to define an internal style sheet?",
+    answer: "<style>",
+    options: [
+      "link",
+      "script",
+      "html",
+      "style"
+    ]
+  },
+  {
+    numb: 8,
+    question: "which of the following property used to change the text color in css?",
+    answer: "color:red;",
+    options: [
+      "text-color:red;",
+      "color:red;",
+      "text:red;",
+      "None of the above"
+    ]
+  },
+  {
+    numb: 9,
+    question: "How to change the text size in css?",
+    answer: "font-size:10px;",
+    options: [
+      "font:20px;",
+      "size:20px;",
+      "text:20px;",
+      "font-size:10px;"
+    ]
+  },
+  {
+    numb: 10,
+    question: "Change the style of the text?",
+    answer: "font-style",
+    options: [
+      "text-style",
+      "font-style",
+      "style",
+      "None of the above"
+    ]
+  },
+  {
+    numb: 11,
+    question: "Which of the following property is used to set the shadow on a box?",
+    answer: "box-shadow",
+    options: [
+      "set-shadow",
+      "shadow",
+      "box-shadow",
+      "None of the above"
+    ]
+  },
+  {
+    numb: 12,
+    question: "Which of the following property is used to set the position of an element in css?",
+    answer: "position",
+    options: [
+      "position",
+      "set",
+      "align",
+      "None of the above"
+    ]
+  },
+  {
+    numb: 13,
+    question: "Which of the following property is used to align the items in table in css?",
+    answer: "text-align",
+    options: [
+      "align",
+      "text-align",
+      "items-align",
+      "None of the above"
+    ]
+  },
+  {
+    numb: 14,
+    question: "Which of the following property allows a marquee to be used in the case of a text-overflow?",
+    answer: "overflow-style",
+    options: [
+      "overflow-marquee",
+      "overflow-style",
+      "overflow-text",
+      "<style>"
+    ]
+  },
+  {
+    numb: 15,
+    question: "Which of the following css property used to defines the space between cells in table?",
+    answer: "border-spacing",
+    options: [
+      "border-spacing",
+      "space-between",
+      "border-style",
+      "None of the above"
+    ]
+  },
+  {
+    numb: 16,
+    question: "Which property is used to change the background color of an element in css?",
+    answer: "background-color",
+    options: [
+      "color",
+      "bg-color",
+      "background-color",
+      "styles"
+    ]
+  },
+  {
+    numb: 17,
+    question: "Which css property is used to change the background image of an element?",
+    answer: "background-image",
+    options: [
+      "background",
+      "background-color",
+      "background-image",
+      "<style>"
+    ]
+  },
+  {
+    numb: 18,
+    question: "Which property is used to remove the underline from hyperlinks in css?",
+    answer: "text-decoration:none;",
+    options: [
+      "text-decoration:none;",
+      "text-underline:none;",
+      "<html>",
+      "text-decoration"
+    ]
+  },
+  {
+    numb: 19,
+    question: "Are negative values allowed in padding in css?",
+    answer: "No",
+    options: [
+      "Yes",
+      "No",
+      "May be",
+      "Can't say"
+    ]
+  },
+  {
+    numb: 20,
+    question: "Are negative values allowed in margins in css?",
+    answer: "Yes",
+    options: [
+      "Yes",
+      "No",
+      "Can't say",
+      "May be"
+    ]
+  },
+  {
+    numb: 21,
+    question: "What's the default value of position in css?",
+    answer: "relative",
+    options: [
+      "absolute",
+      "relative",
+      "Fixed",
+      "static"
+    ]
+  },
+  {
+    numb: 22,
+    question: "Which property is used to control the layout in css?",
+    answer: "Display",
+    options: [
+      "Display",
+      "Header",
+      "Footer",
+      "None of these"
+    ]
+  },
+  {
+    numb: 23,
+    question: "Which CSS property is used to set the stack order of an element?",
+    answer: "Z-index",
+    options: [
+      "Z-index",
+      "position",
+      "overflow",
+      "Clip"
+    ]
+  },
+  {
+    numb: 24,
+    question: "What is the latest version of css3?",
+    answer: "CSS3",
+    options: [
+      "CSS3",
+      "CSS2",
+      "CSS5",
+      "CSS1"
+    ]
+  },
+  {
+    numb: 25,
+    question: "Which property is used to change the color of a text in CSS?",
+    answer: "color",
+    options: [
+      "text-color",
+      "color",
+      "background-color",
+      "background-text-color"
+    ]
+  },
+
 ];
